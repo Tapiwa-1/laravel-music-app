603edf1535a53b21fd9f177356549be106aeb03e
Tapiwa Motsi <tmotsi10@gmail.com>
2022-12-22 20:39:29 +0200

    description and location added

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -42,8 +42,8 @@ class UserController extends Controller
 
             $user->first_name = $request->first_name;
             $user->last_name = $request->last_name;
-            // $user->email = $request->email;
-            // $user->description = $request->description;
+            $user->location = $request->location;
+            $user->description = $request->description;
 
             $user->save();
             return response()->json('User details updated', 200);
